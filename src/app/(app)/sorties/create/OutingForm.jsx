6baa8912e5cde23336860014208b7eca963689d8
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-09-04 21:58:28 +0200

    Create OutingForm.jsx

---
+++ src/app/(app)/sorties/create/OutingForm.jsx
@@ -0,0 +1,12 @@
+// components/OutingForm.jsx
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
+export default function OutingForm() {
+  const searchParams = useSearchParams();
+  // ... le reste de ton code de formulaire
+  return (
+    // ... ton formulaire
+  );
+}
